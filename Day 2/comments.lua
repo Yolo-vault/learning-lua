9c0f85e12ce153abeaa511a83d7a150b55c4b00f
dev-jhon-yo <dev.jhon.ss@gmail.com>
2023-10-03 14:04:57 -0300

    feat: how to comment in lua

---
+++ Day 2/comments.lua
@@ -0,0 +1,16 @@
+-- When commenting a line or Block of code it is ignored, becomming just a comment.
+-- Use VSCode shortcut to comment fast Windows(CTRL+/) or Mac(Cmd+/).
+
+-- Single line commants using two dashes(-) can be:
+print("content") -- example comment next
+-- print("content")
+
+-- Multi-line comments using (--[[ COMMENT HERE ]]):
+
+--[[
+print("content")
+print("content")
+print("content")
+]]
+
+
