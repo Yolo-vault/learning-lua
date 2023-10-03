92e94e5ac74d3a61c55757a2a406df2eda3719fd
dev-jhon-yo <dev.jhon.ss@gmail.com>
2023-10-03 14:09:11 -0300

    feat: opratoes used in lua

---
+++ Day 2/operators.lua
@@ -0,0 +1,43 @@
+-- Lua uses almost all the operators that other programming languages ​​use, examples:
+
+--[[
+  +
+  -
+  *
+  /
+  %
+  ^
+  ==
+  ~=
+  >
+  <
+  >=
+  <=
+  and
+  or
+  not
+  #
+  ..
+]]
+
+-- Addition
+local SUM <const> = 2 + 2
+print("2 + 2 =", SUM)
+
+-- Subtraction
+local SUBTRACTION <const> = 2 - 2
+print("2 - 2 =", SUBTRACTION)
+
+-- Multiplication
+local MULTIPLICATION <const> = 2 * 2
+print("2 * 2 =", MULTIPLICATION)
+
+-- Division
+local DIVISION <const> = 2 / 2
+print("2 / 2 =", DIVISION)
+
+-- Module (Remainder of the division)
+local MODULE <const> = 2 % 2
+print("2 % 2 =", MODULE)
+
+-- We will see the others later!
