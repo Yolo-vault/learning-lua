bb580c25723ea05bf8a61660c9eb8e44e97b6ab0
dev-jhon-yo <dev.jhon.ss@gmail.com>
2023-10-18 11:38:44 -0300

    feat: using the inspect library

--- Day 9/jsonTest.lua
+++ Day 9/jsonTest.lua
@@ -1,12 +1,9 @@
 local json = require("libs.json")
+local inspect = require("libs.inspect")
 
 local file = io.open("resources/example.json", "r")
 local fileContent = file and file:read("a")
 local fileDecode = fileContent and json.decode(fileContent) or {}
 
--- Returns a value representing the decoded JSON string.
 print(fileDecode)
-
-for k, v in pairs(fileDecode) do
-  print(k, v)
-end
+print(inspect(fileDecode))
