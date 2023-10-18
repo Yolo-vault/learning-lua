efd989542b134ea105886fc74ede0edc4dae3f37
dev-jhon-yo <dev.jhon.ss@gmail.com>
2023-10-18 11:44:21 -0300

    feat: improve code with better variable names and comments

--- Day 9/jsonTest.lua
+++ Day 9/jsonTest.lua
@@ -1,9 +1,16 @@
+-- Import the necessary libraries
 local json = require("libs.json")
 local inspect = require("libs.inspect")
 
+-- Try to open the JSON file
 local file = io.open("resources/example.json", "r")
-local fileContent = file and file:read("a")
-local fileDecode = fileContent and json.decode(fileContent) or {}
 
-print(fileDecode)
-print(inspect(fileDecode))
+-- Read the file content if it was opened successfully
+local fileContent = file and file:read("a")
+
+-- Decode the JSON content into a Lua table, or create an empty table if the file is empty or doesn't exist
+local fileData = fileContent and json.decode(fileContent) or {}
+
+-- Print the resulting Lua table using the inspect library
+print("File content using the inspect library:")
+print(inspect(fileData))
