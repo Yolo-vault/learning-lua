ed12e7402d56d37b2aebe4cfaa79bae5cc3e10c4
dev-jhon-yo <dev.jhon.ss@gmail.com>
2023-10-25 14:48:36 -0300

    feat: applying typing to codes

--- Day 12/choice.lua
+++ Day 12/choice.lua
@@ -1,17 +1,16 @@
 local class = require("libs.middleclass")
 
+---@class Choice
 local Choice = class("Choice")
 
 function Choice:initialize()
-  self.description = nil
-  self.destination = nil
+  self.description = nil ---@type string
+  self.destination = nil ---@type string
   self.condition = nil
 end
 
---
 ---Returns whether the choice has a condition to be executed.
 ---@return boolean
---
 function Choice:hasCondition()
   return self.condition ~= nil
 end
--- Day 12/node.lua
+++ Day 12/node.lua
@@ -1,13 +1,14 @@
 local class = require("libs.middleclass")
 
+---@class Node
 local Node = class("Node")
 
 function Node:initialize(id)
-  self.id = id
-  self.title = nil
-  self.description = nil
-  self.header = nil
-  self.choices = {}
+  self.id = id ---@type string
+  self.title = nil ---@type string
+  self.description = nil ---@type string
+  self.header = nil ---@type string
+  self.choices = {} ---@type Choice[]
 end
 
 return Node
